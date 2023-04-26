2e7cc8f032095c48b86d4654825413007dd30d14
Bassem <bassemsaeed620@gmail.com>
2023-04-26 05:10:01 +0200

    openEnrolledCourse

--- controllers/learningController.php
+++ controllers/learningController.php
@@ -8,7 +8,7 @@ class Course{
         if($this->db->openConnection()){
             $query = "select * from courses";
             $result = $this->db->select($query);
-            if(!$result)
+            if(!$result || count($result) == 0)
             {
                 return false;
             }
@@ -18,16 +18,22 @@ class Course{
         return false;
     }
 
-    public function getCourse($id){
+    public function openEnrolledCourse($course_id, $user_id){
         $this->db = new DBController;
         if($this->db->openConnection()){
-            $query = "select * from courses where id = $id";
-            $result = $this->db->select($query);
-            if(!$result)
+            $query1 = "select * from courses_users where course_id = $course_id AND user_id = $user_id";
+            $result1 = $this->db->select($query1);
+            if(!$result1 || count($result1) == 0)
             {
                 return false;
             }
-            return $result;
+            $query2 = "select * from courses where id = $course_id";
+            $result2 = $this->db->select($query2);
+            if(!$result2 || count($result2) == 0)
+            {
+                return false;
+            }
+            return $result2;
         }
         echo "error in connection";
         return false;
