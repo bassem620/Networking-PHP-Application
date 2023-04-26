7b830d7550376db671873a924a836235c1c579d4
Bassem <bassemsaeed620@gmail.com>
2023-04-26 04:15:35 +0200

    tabs

--- controllers/learningController.php
+++ controllers/learningController.php
@@ -1,16 +1,17 @@
 <?php
 require_once "../../controllers/DBController.php";
 class courses{
- protected $db;
- public function getCourses(){
-    $this->db = new DBController;
-    if($this->db->openConnection()){
-        $query = "select * from courses";
-        $result = $this->db->select($query);
-        return $result;
+    protected $db;
+
+    public function getCourses(){
+        $this->db = new DBController;
+        if($this->db->openConnection()){
+            $query = "select * from courses";
+            $result = $this->db->select($query);
+            return $result;
+        }
+        echo "error in connection";
+        return;
     }
-    echo "error in connection";
-    return;
- }
 }
 ?>
