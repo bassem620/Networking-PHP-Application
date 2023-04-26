087dc06e8771a980214350ba83a4c2abff1fa8cc
beshoy bahaa <bahaa_beshoy@yahoo.com>
2023-04-26 04:12:51 +0200

    get courses from database

---
+++ controllers/learningController.php
@@ -0,0 +1,16 @@
+<?php
+require_once "../../controllers/DBController.php";
+class courses{
+ protected $db;
+ public function getCourses(){
+    $this->db = new DBController;
+    if($this->db->openConnection()){
+        $query = "select * from courses";
+        $result = $this->db->select($query);
+        return $result;
+    }
+    echo "error in connection";
+    return;
+ }
+}
+?>
