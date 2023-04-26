31a893e2426944d1a9328805e4f2c60a546a5abd
beshoy bahaa <bahaa_beshoy@yahoo.com>
2023-04-26 04:53:57 +0200

    home test

--- views/home.php
+++ views/home.php
@@ -1,14 +1,15 @@
 <?php
-    require_once "../controllers/premiumController.php";
-    require_once "../models/users/premium.php";
-
-    if(!isset($_SESSION["id"]))
-    {
-        session_start();
-    }
+session_start();
+    
 
     echo $_SESSION["id"];
     echo "<br/>";
     echo $_SESSION["email"];
+    require_once "../controllers/learningController.php";
+    
+    $db = new courses;
+    $result = $db->getCourses();
+    echo $result;
 
+    
 ?>
