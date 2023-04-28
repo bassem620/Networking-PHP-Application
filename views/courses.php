c740ddea0c2877103e1aef40c1f7341c09806697
beshoy bahaa <bahaa_beshoy@yahoo.com>
2023-04-29 02:56:49 +0300

    courses front

---
+++ views/courses.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Courses - Mentor Bootstrap Template</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <!-- =======================================================
+  * Template Name: Mentor
+  * Updated: Mar 10 2023 with Bootstrap v5.2.3
+  * Template URL: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+    <main id="main" data-aos="fade-in">
+
+        <!-- ======= Breadcrumbs ======= -->
+        <div class="breadcrumbs">
+            <div class="container">
+                <h2>Courses</h2>
+            </div>
+        </div><!-- End Breadcrumbs -->
+
+
+    <?php
+        require_once "../controllers/learningController.php";
+        $result=$this->getCourses();
+        while($row = $result->fetch_assoc()) {
+echo "
+ <!-- ======= Courses Section ======= -->
+        <section id=\"courses\" class=\"courses\">
+            <div class=\"container\" data-aos=\"fade-up\">
+
+                <div class=\"row\" data-aos=\"zoom-in\" data-aos-delay=\"100\">
+
+                    <div class=\"col-lg-4 col-md-6 d-flex align-items-stretch\">
+                        <div class=\"course-item\">
+                            <img src=\"assets/img/course-1.jpg\" class=\"img-fluid\" alt=\"...\">
+                            <div class=\"course-content\">
+                                <div class=\"d-flex justify-content-between align-items-center mb-3\">
+                                    <button type=\"button\" class=\"btn btn-success\"
+                                        onclick=\"enrolCourse()\">Enroll</button>
+                                    <!--<p class=\"price\">$169</p>-->
+                                    <p class=\"price\">".$row["price"]."</p>
+                                    <h3><a href=\"course-details.html\">". $row["name"]."</a></h3>
+                                <p>".$row["desc"]."</p>
+                            </div>
+                        </div>
+                    </div> <!-- End Course Item-->";
+
+        /*<!-- ======= Courses Section ======= -->
+        <section id="courses" class="courses">
+            <div class="container" data-aos="fade-up">
+
+                <div class="row" data-aos="zoom-in" data-aos-delay="100">
+
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                        <div class="course-item">
+                            <img src="assets/img/course-1.jpg" class="img-fluid" alt="...">
+                            <div class="course-content">
+                                <div class="d-flex justify-content-between align-items-center mb-3">
+                                    <button type="button" class="btn btn-success"
+                                        onclick="enrolCourse()">Enroll</button>
+                                    <!--<p class="price">$169</p>-->*/
+                                    
+                                    
+                                    
+                                /*</div>
+
+                                <h3><a href="course-details.html">Website Design</a></h3>
+                                <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae
+                                    dolores dolorem tempore.</p>
+                            </div>
+                        </div>
+                    </div> <!-- End Course Item-->*/
+}
+                                    ?>
+
+                </div>
+
+            </div>
+        </section><!-- End Courses Section -->
+
+    </main><!-- End #main -->
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
