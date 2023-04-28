c88c3216f12fc4b10ddc87ecbabc9ef49cfb2ecd
Bassem <bassemsaeed620@gmail.com>
2023-04-28 23:10:16 +0300

    cert class

---
+++ models/profile/certificate.php
@@ -0,0 +1,13 @@
+<?php
+
+class Certificate
+{
+    public $id;
+    public $user_id;
+    public $name;
+    public $organization;
+    public $issue_date;
+    public $exp_date;
+    public $cred_id;
+    public $cred_url;
+}
