9f51021e49a24bf4ed1f154f0ffc0f6684bd9d0b
Bassem <bassemsaeed620@gmail.com>
2023-04-28 23:42:01 +0300

    Footer and header and home edits

--- views/home.php
+++ views/home.php
@@ -22,9 +22,49 @@
     // $Premium = new PremiumConrtroller;
     // $Premium->hideConnections($pr);
 
-    $usr = new User;
-    $usr->id = $_SESSION["id"];
-    $usrCont = new UserController;
-    $usrCont->upgradeToPremium($usr);
+    // $usr = new User;
+    // $usr->id = $_SESSION["id"];
+    // $usrCont = new UserController;
+    // $usrCont->upgradeToPremium($usr);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <title>LinkedIn - Home</title>
+</head>
+<body>
+    <?php require_once "components/header.php" ?>
+    <div></div>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+    <?php require_once "components/footer.php" ?> 
+</body>
+</html>
