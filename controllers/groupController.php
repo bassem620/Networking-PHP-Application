2ffbecb39c1de3b732eea082ff9bf02393412c66
Bassem <bassemsaeed620@gmail.com>
2023-04-30 19:36:07 +0300

    group 1

--- controllers/groupController.php
+++ controllers/groupController.php
@@ -5,15 +5,13 @@ class GroupController
 {
     protected $db;
 
-    public function createGroup(Group $grp)
+    public function createGroup($user_id, Group $grp)
     {
         $this->db = new DBController;
-        if($this->db->openConnection())
-        {
-            $query = "INSERT INTO group values ('', '$grp->name', 0)";
+        if ($this->db->openConnection()) {
+            $query = "INSERT INTO group values ('', '$grp->name', 0, '$user_id')";
             $result = $this->db->insert($query);
-            if(!$result)
-            {
+            if (!$result) {
                 return false;
             }
             return true;
@@ -25,12 +23,10 @@ class GroupController
     public function deleteGroup($grp_id)
     {
         $this->db = new DBController;
-        if($this->db->openConnection())
-        {
+        if ($this->db->openConnection()) {
             $query = "DELETE FROM groups WHERE id = '$grp_id'";
             $result = $this->db->delete($query);
-            if(!$result)
-            {
+            if (!$result) {
                 return  false;
             }
             return true;
@@ -42,12 +38,10 @@ class GroupController
     public function joinGroup($user_id, $group_id)
     {
         $this->db = new DBController;
-        if($this->db->openConnection())
-        {
+        if ($this->db->openConnection()) {
             $query = "INSERT INTO joined_groups values ('$user_id', '$group_id')";
             $result = $this->db->insert($query);
-            if(!$result)
-            {
+            if (!$result) {
                 return  false;
             }
             return true;
@@ -55,6 +49,50 @@ class GroupController
         echo "Error in database connection";
         return false;
     }
-}
 
-?>
+    public function getAllGroups()
+    {
+        $this->db = new DBController;
+        if ($this->db->openConnection()) {
+            $query = "SELECT * FROM groups";
+            $result = $this->db->select($query);
+            if (!$result) {
+                return false;
+            }
+            return $result;
+        }
+        echo "Error in database connection";
+        return false;
+    }
+
+    public function getJoinedGroups($user_id)
+    {
+        $this->db = new DBController;
+        if ($this->db->openConnection()) {
+            $query = "SELECT * from groups INNER JOIN joined_groups ON id=group_id WHERE user_id='$user_id'";
+            $result = $this->db->select($query);
+            if (!$result) {
+                return false;
+            }
+            return $result;
+        }
+        echo "Error in database connection";
+        return false;
+    }
+
+    public function getMyGroups($user_id)
+    {
+        $query = "SELECT * from groups WHERE user_id='$user_id'";
+        $this->db = new DBController;
+        if ($this->db->openConnection()) {
+            $query = "SELECT * from groups WHERE user_id='$user_id'";
+            $result = $this->db->select($query);
+            if (!$result) {
+                return false;
+            }
+            return $result;
+        }
+        echo "Error in database connection";
+        return false;
+    }
+}
---
+++ views/createGroup.php
@@ -0,0 +1,84 @@
+<?php
+
+require_once "../models/group.php";
+require_once "../controllers/groupController.php";
+if (!isset($_SESSION["id"])) {
+    session_start();
+}
+
+// Groups
+$groupCont = new GroupController;
+
+$errMsg = "";
+
+if (isset($_POST['name']) && isset($_POST['state'])) {
+    if (!empty($_POST['name']) && !empty($_POST['state'])) {
+        $group = new Group;
+        $controller = new GroupController;
+
+        $group->name = $_POST['name'];
+        $group->state = "";
+        if ($_POST['state'] == "public" || $_POST['state'] == "Public") {
+            $group->state = 0;
+        } else {
+            $group->state = 1;
+        }
+
+        if (!isset($_SESSION["id"])) {
+            session_start();
+        }
+
+        if (!$controller->createGroup($_SESSION["id"], $group)) {
+            $errMsg = $_SESSION["errMsg"];
+        } else {
+            header("location: /linkedIn/views/groups.php");
+        }
+    } else {
+        $errMsg = "Invalid credentials";
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LinkedIn - Create group</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <div class="container my-5">
+        <h2>New group</h2>
+
+        <form method="POST" action="group.php">
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Group name</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="name">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">State</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="state">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <div class="offset-sm-3 col-sm-3 d-grid">
+                    <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+                </div>
+                <div class="col-sm-3 d-grid">
+                    <a class="btn btn-outline-primary" href="/linkedIn/views/groups.php" role="button">Cancel</a>
+                </div>
+            </div>
+        </form>
+</body>
+
+</html>
