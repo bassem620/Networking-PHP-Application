41aa12bebd200dcc3f0cb5580d65476103f22251
Bassem <bassemsaeed620@gmail.com>
2023-05-01 18:18:35 +0300

    home edits

--- views/home.php
+++ views/home.php
@@ -1,9 +1,5 @@
 <?php
 require_once "../controllers/userController.php";
-require_once "../controllers/premiumController.php";
-require_once "../controllers/learningController.php";
-require_once "../controllers/DBController.php";
-require_once "../models/users/premium.php";
 
 // Check Session
 if (!isset($_SESSION["id"])) {
@@ -14,40 +10,15 @@ if (!isset($_SESSION["id"])) {
    }
 }
 
-echo $_SESSION["id"];
-echo "<br/>";
-echo $_SESSION["email"];
-
-// $pdf = new PremiumConrtroller;
-// $result = $pdf->exportConnections($_SESSION["id"]);
-
-// $db = new Course;
-// $result = $db->getCourses();
-// print_r($result);
-
-// $pr = new Premium;
-// $pr->id = $_SESSION["id"];
-// $Premium = new PremiumConrtroller;
-// $Premium->hideConnections($pr);
-
-// $usr = new User;
-// $usr->id = $_SESSION["id"];
-// $usrCont = new UserController;
-// $usrCont->upgradeToPremium($usr);
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <!-- Google Fonts -->
    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-   <!-- Vendor CSS Files -->
    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -55,8 +26,6 @@ echo $_SESSION["email"];
    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-   <!-- Template Main CSS File -->
    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
    <link href="assets/css/style.css" rel="stylesheet">
    <link href="../views/assets/css_beshoy/stryle.css" rel="stylesheet">
@@ -64,40 +33,21 @@ echo $_SESSION["email"];
 </head>
 
 <body>
-   <?php require_once "components/header.php"
-   ?>
-   <div></div>
-
+   <?php require_once "components/header.php"; ?>
 
    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-   <div class="container">
+   <div class="container-lg">
       <div class="row">
          <div class="col-md-12">
             <div id="content" class="content content-full-width">
-
-               <!-- begin profile-content -->
                <div class="profile-content">
-                  <!-- begin tab-content -->
                   <div class="tab-content p-0">
-                     <!-- begin #profile-post tab -->
                      <div class="tab-pane fade active show" id="profile-post">
-                        <!-- begin timeline -->
                         <ul class="timeline">
                            <li>
-
-                              <!-- end timeline-time -->
-                              <!-- begin timeline-icon -->
-                              <div class="timeline-icon">
-                                 <a href="javascript:;">&nbsp;</a>
-                              </div>
-
-                              <!-- end timeline-icon -->
-                              <!-- begin timeline-body -->
                               <div class="timeline-body">
                                  <div class="timeline-header">
-
-                                    <span class="username"><a href="javascript:;">name</a> <small></small></span>
-                                    <span class="pull-right text-muted">000Views</span>
+                                    <span class="username"><a href="javascript:;">name</a></span>
                                  </div>
                                  <div class="timeline-content">
                                     <p>
@@ -106,28 +56,20 @@ echo $_SESSION["email"];
                                  </div>
                                  <div class="timeline-likes">
                                     <div class="stats-right">
-                                       <span class="stats-text">000Shares</span>
                                        <span class="stats-text">0000Comments</span>
                                     </div>
                                     <div class="stats">
-                                       <span class="fa-stack fa-fw stats-icon">
-                                          <i class="fa fa-circle fa-stack-2x text-danger"></i>
-                                          <i class="fa fa-heart fa-stack-1x fa-inverse t-plus-1"></i>
-                                       </span>
+                                       <span class="stats-total">000k</span>
                                        <span class="fa-stack fa-fw stats-icon">
                                           <i class="fa fa-circle fa-stack-2x text-primary"></i>
                                           <i class="fa fa-thumbs-up fa-stack-1x fa-inverse"></i>
                                        </span>
-                                       <span class="stats-total">000k</span>
                                     </div>
                                  </div>
                                  <div class="timeline-footer">
                                     <a href="javascript:;" class="m-r-15 text-inverse-lighter"><i class="fa fa-thumbs-up fa-fw fa-lg m-r-3"></i> Like</a>
-                                    <a href="javascript:;" class="m-r-15 text-inverse-lighter"><i class="fa fa-comments fa-fw fa-lg m-r-3"></i> Comment</a>
-                                    <a href="javascript:;" class="m-r-15 text-inverse-lighter"><i class="fa fa-share fa-fw fa-lg m-r-3"></i> Share</a>
                                  </div>
                                  <div class="timeline-comment-box">
-
                                     <div class="input">
                                        <form action="">
                                           <div class="input-group">
@@ -140,30 +82,21 @@ echo $_SESSION["email"];
                                     </div>
                                  </div>
                               </div>
-                              <!-- end timeline-body -->
                            </li>
                         </ul>
-                        <!-- end timeline -->
                      </div>
-                     <!-- end #profile-post tab -->
                   </div>
-                  <!-- end tab-content -->
                </div>
-               <!-- end profile-content -->
             </div>
          </div>
       </div>
    </div>
 
-
-   <!-- Vendor JS Files -->
    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
    <script src="assets/vendor/aos/aos.js"></script>
    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
    <script src="assets/vendor/php-email-form/validate.js"></script>
-
-   <!-- Template Main JS File -->
    <script src="assets/js/main.js"></script>
    <?php require_once "components/footer.php"
    ?>
