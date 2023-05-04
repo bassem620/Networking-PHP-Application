599be049be04d2e01e8f889c9098e22671ddd207
Bassem <bassemsaeed620@gmail.com>
2023-05-04 22:23:52 +0300

    skills insertion SQL

---
+++ INSERTION.sql
@@ -0,0 +1,15 @@
+-- Skills
+INSERT INTO skills (name) VALUES 
+('Java'),
+('Python'),
+('C++'),
+('JavaScript'),
+('SQL'),
+('Data Structures'),
+('Algorithms'),
+('Object-Oriented Programming'),
+('Operating Systems'),
+('Computer Networks'),
+('Databases'),
+('Web Development'),
+('Mobile Development');
