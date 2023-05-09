ee645aef387130e003b38830b3e54eeab504311a
Bassem <bassemsaeed620@gmail.com>
2023-05-09 20:01:32 +0300

    comments removed

--- controllers/premiumController.php
+++ controllers/premiumController.php
@@ -47,14 +47,8 @@ class PremiumConrtroller
         if ($this->db->openConnection()) {
             $profileConnection = new ProfileController;
             $result = $profileConnection->getConnections($user_id);
-            // $query1 = "SELECT `users`.`firstName`, `users`.`lastName` FROM users INNER join connections on `users`.`id`= `connections`.`user2_id` where `connections`.`user1_id` = '$user_id';";
-            // $result1 = $this->db->select($query1);
-            // $query2 = "SELECT `users`.`firstName`, `users`.`lastName` FROM users INNER join connections on `users`.`id`= `connections`.`user1_id` where `connections`.`user2_id` = '$user_id';";
-            // $result2 = $this->db->select($query2);
-            // $result = array_merge($result1, $result2);
             ob_end_clean();
             $pdf = new FPDF();
-            // Define alias for number of pages
             $pdf->AddPage();
             $pdf->SetFont('Times', '', 14);
             $counter = 1;
