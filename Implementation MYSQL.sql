c582905a66278ec9eddbe236579784bc3e8d9fc2
Bassem <bassemsaeed620@gmail.com>
2023-05-09 19:45:40 +0300

    Implementation MYSQL

---
+++ Implementation MYSQL.sql
@@ -0,0 +1,204 @@
+CREATE TABLE `users` (
+  `id` int AUTO_INCREMENT NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `email` varchar(50) UNIQUE NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `birthday` date,
+  `phone` varchar(10),
+  `profile_type` int NOT NULL,
+  `about` varchar(300),
+  `open_to` varchar(10),
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `premium`(
+  `user_id` int NOT NULL,
+  `start_date` date NOT NULL,
+  `exp_date` date NOT NULL,
+  PRIMARY KEY (`user_id`),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE `websites` (
+  `id` int AUTO_INCREMENT NOT NULL,
+  `user_id` int NOT NULL,
+  `link` varchar(100) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE `educations` (
+  `id` int AUTO_INCREMENT NOT NULL,
+  `user_id` int NOT NULL,
+  `school` varchar(50) NOT NULL,
+  `degree` varchar(50) NOT NULL,
+  `field_of_study` varchar(50) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date,
+  `grade` varchar(50),
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE `positions` (
+  `id` int AUTO_INCREMENT NOT NULL,
+  `user_id` int NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `type` varchar(50),
+  `company` varchar(50) NOT NULL,
+  `location` varchar(100),
+  `start_date` date NOT NULL,
+  `end_date` date,
+  `currently_working` boolean DEFAULT 1 NOT NULL,
+  `industry` varchar(50),
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE `certifications` (
+  `id` int AUTO_INCREMENT NOT NULL,
+  `user_id` int  NOT NULL,
+  `name` varchar(50)  NOT NULL,
+  `organization` varchar(50),
+  `issue_date` date  NOT NULL,
+  `exp_date` date,
+  `cred_id` varchar(50)  NOT NULL,
+  `cred_url` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE `connections` (
+  `user1_id` int,
+  `user2_id` int,
+  `state` enum('0', '1') NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user1_id`, `user2_id`),
+  FOREIGN KEY (user1_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (user2_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE `skills` (
+  `id` int AUTO_INCREMENT NOT NULL,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `user_skills` (
+  `user_id` int NOT NULL,
+  `skill_id` int  NOT NULL,
+  PRIMARY KEY (`user_id`, `skill_id`),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (skill_id) REFERENCES skills(id) ON DELETE CASCADE
+);
+
+CREATE TABLE `endorses` (
+  `user1_id` int NOT NULL,
+  `user2_id` int NOT NULL,
+  `skill_id` int NOT NULL,
+  PRIMARY KEY (`user1_id`, `user2_id`, `skill_id`),
+  FOREIGN KEY (user1_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (user2_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (skill_id) REFERENCES skills(id) ON DELETE CASCADE
+);
+
+CREATE TABLE `jobs` (
+  `id` int AUTO_INCREMENT NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `desc` varchar(100)  NOT NULL,
+  `req.` varchar(100),
+  `salary` int NOT NULL,
+  `company` varchar(50),
+  `location` varchar(50),
+  `creator_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (creator_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE `applied_jobs` (
+  `user_id` int,
+  `job_id` int,
+  PRIMARY KEY (`user_id`, `job_id`),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (job_id) REFERENCES jobs(id) ON DELETE CASCADE
+);
+
+CREATE TABLE `groups` (
+  `id` int AUTO_INCREMENT NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `desc` varchar(300) NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE `joined_groups` (
+  `user_id` int,
+  `group_id` int,
+  PRIMARY KEY (`user_id`, `group_id`),
+  FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY(group_id) REFERENCES groups(id) ON DELETE CASCADE
+);
+
+CREATE TABLE `events` (
+  `id` int AUTO_INCREMENT NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `desc` varchar(200),
+  `date` date NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE `events_Going` (
+  `user_id` int,
+  `event_id` int,
+  PRIMARY KEY (`user_id`, `event_id`),
+  FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY(event_id) REFERENCES `events`(id) ON DELETE CASCADE
+);
+
+CREATE TABLE `posts` (
+  `id` int AUTO_INCREMENT NOT NULL,
+  `user_id` int NOT NULL,
+  `group_id` int,
+  `desc` varchar(1000) ,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(group_id) REFERENCES `groups`(id) ON DELETE CASCADE
+);
+
+CREATE TABLE `post_comments` (
+  `post_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `comment`  varchar(200) NOT NULL,
+  FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+  FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE `post_Likes` (
+  `post_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`post_id`, `user_id`),
+  FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY(post_id) REFERENCES posts(id) ON DELETE CASCADE
+);
+
+CREATE TABLE `courses` (
+  `course_id` int AUTO_INCREMENT NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `desc` varchar(100),
+  `skills` varchar(100),
+  `hours` int,
+  `price` int NOT NULL,
+  `url` varchar(500) NOT NULL,
+  PRIMARY KEY (`course_id`)
+);
+
+CREATE TABLE `courses_users` (
+  `course_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`course_id`, `user_id`),
+  FOREIGN KEY(course_id) REFERENCES courses(course_id) ON DELETE CASCADE,
+  FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+);
